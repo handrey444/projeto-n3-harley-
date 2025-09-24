a5f572c14dfa62841cbdd4f786ee00d4e572e3f4
Handrey Carvalho Galdino <149391089+handrey444@users.noreply.github.com>
2025-09-24 16:36:01 -0300

    Corrigindo erro no vercel

--- back-end/index.js
+++ back-end/index.js
@@ -1,12 +1,22 @@
 const express = require('express');
 const cors = require('cors');
 const router = require('./routes/routes');
+const path = require('path');
+
 
 const app = express();
 const port =  3000;
 
 app.use(cors());
-app.use('/', router);
+
+
+
+
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, '..', 'front-end', 'index.html'));
+});
+
+
 
 app.listen(port, () => {
     console.log(`Servidor rodando em http://localhost:${port}`);
